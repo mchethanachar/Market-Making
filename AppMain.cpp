bdc07e2452773c3af1445f896b2f05a8a8d5c8d1
mchethanachar <mchethan.achar@gmail.com>
2024-06-17 18:43:45 +0530

    Candle stick data

--- AppMain.cpp
+++ AppMain.cpp
@@ -13,42 +13,60 @@ int main()
 
 void readTradeFile()
 {
+	/* All variables */
 	fstream trades_file;
+	string tradeLine,timestr;
+	int i = 0;
+	char x;
+	int h = -1;
+	int n = 0;
+	int min = 0;
+	int hour = 0;
+	int prevMin = 0;
+	int totalMin = 0;
+	float midPrice = 0;
+	float predictedPrice = 0;
+	float sd = 0;
+	float open, high, low, close;
+
 	// Create and open a text file
     ofstream priceFile("price_file.csv");
     ofstream maFile("ma_file.csv");
 	ofstream predictedPriceFile("predictedPrice_file.csv");
+	ofstream candleStick("candleStick_file.csv");
 	vector<string> orderDetails;
-	trades_file.open("D:/HFT/Data/EQUITY_ORDER_V3_2023-10-04/wipro.txt",ios::in);
+	trades_file.open("D:/HFT/Data/EQUITY_ORDER_V3_2023-10-04/infosys.txt",ios::in);
 	if(trades_file.is_open())
 	{
 		cout<<"File is open\n";
-		string tradeLine,timestr;
-		int i = 0;
-		char x;
-		int h = -1;
-		int n = 0;
-		int min = 0;
-		int hour = 0;
-		int prevMin = 0;
-		int totalMin = 0;
-		float midPrice = 0;
-		float predictedPrice = 0;
-		float sd = 0;
 		while(getline(trades_file, tradeLine))
 		{
 			i++;
 			OrderItem orderItem= getOrderItem(tradeLine);
 			//orderItem.details();
 			processOrder(orderItem);
-			if(preOpen == false)
+			//if(preOpen == false)
 			//updateMmOrders();
 			min = getMin(orderItem.versionTime);
 			hour = getHour(orderItem.versionTime);
 			totalMin = getTotalMin(hour, min);
+			midPrice = getMidPrice(bestAsk, bestBid);
+
+			if(midPrice>high)
+			high = midPrice;
+
+			if(midPrice<low)
+			low = midPrice;
+
 			if(min != prevMin && preOpen == false && totalMin >= 15)
 			{
-				midPrice = getMidPrice(bestAsk, bestBid);
+				close = midPrice;
+				if(totalMin>15)
+				candleStick<<totalMin-1<<","<<open<<","<<high<<","<<low<<","<<close<<"\n";
+				open = midPrice;
+				close = midPrice;
+				high = midPrice;
+				low = midPrice;
 				prevMin = getMin(orderItem.versionTime);
 				priceFile<<totalMin<<","<<midPrice<<"\n";
 				//printBook(bidBook, askBook, bestBid, bestAsk, totalBids, totalAsks);
@@ -93,6 +111,7 @@ void readTradeFile()
 		priceFile.close();
 		maFile.close();
 		predictedPriceFile.close();
+		candleStick.close();
 		cin>>x;
 	}
 	cout<<"File reading complete";
